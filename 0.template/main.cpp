ce0dc37b4c8fe1bd782cf74b656d16ea7b7cef43
0xlitf <0xlitf@gmail.com>
2023-11-11 15:11:40 +0800

    ·

--- 0.template/main.cpp
+++ 0.template/main.cpp
@@ -1,14 +1,13 @@
 #include <QCoreApplication>
 #include <iostream>
-#include <vector>
 #include <memory>
-#include <tuple>
-#include <string>
-#include <stdexcept>
 #include <set>
+#include <stdexcept>
+#include <string>
+#include <tuple>
+#include <vector>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
     QCoreApplication a(argc, argv);
     auto&& appFilePath = QCoreApplication::applicationFilePath();
     qDebug() << "starting " << appFilePath;
